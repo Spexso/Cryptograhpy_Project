561f7dffc45ba58bf603bcced65f3ec790390f7d
Kağan <89082422+Spexso@users.noreply.github.com>
2024-12-23 03:34:55 +0300

    initial commit

---
+++ SieveAtkin.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class SieveAtkin
+{
+public:
+	SieveAtkin();
+	~SieveAtkin();
+
+private:
+
+};
+
+SieveAtkin::SieveAtkin()
+{
+	// Default Constructor
+}
+
+SieveAtkin::~SieveAtkin()
+{
+	// Default Destructor
+}
+
